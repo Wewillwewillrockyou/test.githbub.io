139256468ea137500012a2abd6ff51b9a0d49da8
spdspdspd <90323605+Wewillwewillrockyou@users.noreply.github.com>
2021-09-08 22:09:50 +0545

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,2 @@
 console.log(1+6+9)
-alert(document.cookie)
-window.onload = function() {
-    // similar behavior as clicking on a link
-    window.location.href = "http://stackoverflow.com";
-}
+console.log(document.cookie)
